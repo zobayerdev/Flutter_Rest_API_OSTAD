599adca9b74fe741c2615842484b80f11c392a68
zobayerdev <zobayer.dev@gmail.com>
2024-05-20 20:08:32 +0600

    Upload project

---
+++ android/app/src/main/kotlin/com/example/get_data_from_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.get_data_from_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/view/app.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home screen/home_screen.dart';
+
+class PhotoGallery extends StatelessWidget {
+  const PhotoGallery({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Photo Gallery',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
